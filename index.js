e3943170c29851aa4a69cfa7a0653be34f154749
Daniel <pdaniel2005@gmail.com>
2014-04-13 14:32:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var http = require('http')
+    , url = require('url');
+
+var angularseo = module.exports = function(req,res,next){
+    console.log("am intrat");
+    if (!req.originalUrl.match(/_escaped_fragment/)) return next();
+
+};
+
+
+
+module.exports = {
+  escape: function(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  },
+
+  /**
+   * Unescape special characters in the given string of html.
+   *
+   * @param  {String} html
+   * @return {String}
+   */
+  unescape: function(html) {
+    return String(html)
+      .replace(/&amp;/g, '&')
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, '\'')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+  }
+};
